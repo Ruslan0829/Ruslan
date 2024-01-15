e7a46c63a37d8587b61a4a0e7ef641d6a19aa32f
Ruslan0829 <151495306+Ruslan0829@users.noreply.github.com>
2024-01-15 11:25:26 +0500

    Add files via upload

---
+++ ruslanurok32/script.js
@@ -0,0 +1,6 @@
+// let a = 49
+// let stat = ( a % 2 == 0 ) ? 'Четное' : "Нечетное"
+// console.log(stat);
+let b = prompt('СКОЛЬКО ВАМ ЛЕТ?')
+let Otvet = (b >= 18) ? 'Ты уже совершенно-летний' : 'Ты малыш'
+console.log(Otvet);
