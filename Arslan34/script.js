26832cbbb8f591fd4f5480a098d322d3f2ad5297
Ruslan0829 <151495306+Ruslan0829@users.noreply.github.com>
2024-01-15 11:30:44 +0500

    Add files via upload

---
+++ Arslan34/script.js
@@ -0,0 +1,11 @@
+// let a = 21
+
+
+// let stat = (a % 2 == 0) ? 'верное' : 'не верное'
+
+
+// console.log(stat);
+
+let a = prompt('Сколько вам лет')
+let answer = (a > 16 ) ? 'Взрослый' : 'Маленький бегимотик'
+console.log(answer);
